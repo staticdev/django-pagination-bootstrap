2fffae61fd5e9c1d95b8ebd00c34fe700d8efc9b
staticdev <thiagocavila@gmail.com>
2013-07-04 21:21:14 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import pagination_bootstrap
+
+import os
+import codecs
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+setup(
+    name='django-pagination-bootstrap',
+    version=pagination_bootstrap.__version__,
+    maintainer='Thiago Carvalho D Avila',
+    maintainer_email='thiagocavila@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[],
+    url='https://github.com/staticdev/django-pagination-bootstrap',
+    license='LICENSE',
+    description='Easy add pagination in Django, using Twitter Bootstrap's layout.',
+    long_description=read('README.rst'),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        "Django >= 1.4.0"
+    ],
+)
