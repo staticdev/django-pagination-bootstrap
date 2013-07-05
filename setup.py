618190a794b0f7b8ff3a913e865e97ccd633ee45
staticdev <thiagocavila@gmail.com>
2013-07-04 21:53:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     scripts=[],
     url='https://github.com/staticdev/django-pagination-bootstrap',
     license='LICENSE',
-    description='Easy add pagination in Django, using Twitter Bootstrap's layout.',
+    description="Easy add pagination in Django, using Twitter Bootstrap's layout.",
     long_description=read('README.rst'),
     classifiers=[
         "Environment :: Web Environment",
