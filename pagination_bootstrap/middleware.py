0d64a1706f36c17147acc8963ae7e41201314f07
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-03-08 17:42:34 +0100

    Django 1.10 adaptation

--- pagination_bootstrap/middleware.py
+++ pagination_bootstrap/middleware.py
@@ -1,3 +1,6 @@
+from django.utils.deprecation import MiddlewareMixin
+
+
 def get_page(self):
     """
     A function which will be monkeypatched onto the request to get the current
@@ -9,13 +12,10 @@ def get_page(self):
         return 1
 
 
-class PaginationMiddleware(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
+class PaginationMiddleware(MiddlewareMixin):
     """
     Inserts a variable representing the current page onto the request object if
     it exists in either **GET** or **POST** portions of the request.
     """
-    def __call__(self, request):
+    def process_request(self, request):
         request.__class__.page = property(get_page)
-        return self.get_response(request)
