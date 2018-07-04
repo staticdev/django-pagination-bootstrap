0cffa62d78c541ce7477b9084d88fa9155e7f4fe
Urtzi Odriozola <uodriozola@codesyntax.com>
2018-07-04 10:32:07 +0200

    setup ajusted to be Django 1.11 compatible

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ import pagination_bootstrap
 import os
 import codecs
 import uuid
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
