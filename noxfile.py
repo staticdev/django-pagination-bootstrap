39d13d6b3c17507318d52f87d2136bbc2753621e
staticdev <thiagocavila@gmail.com>
2021-01-14 06:02:10 +0100

    New deps

--- noxfile.py
+++ noxfile.py
@@ -10,13 +10,14 @@ from nox.sessions import Session
 
 
 package = "django_pagination_bootstrap"
-python_versions = ["3.9", "3.8", "3.7", "3.6"]
+python_versions = ["3.9", "3.8", "3.7"]
 nox.options.sessions = (
     "pre-commit",
     "safety",
     "mypy",
     "tests",
     "typeguard",
+    "xdoctest",
     "docs-build",
 )
 
